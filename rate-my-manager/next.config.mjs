ada257518051beffa1603c97c5d4ce4b7b499978
Arsh Gupta <arsh121523@gmail.com>
2025-02-18 10:18:06 -0500

    Added image

--- rate-my-manager/next.config.mjs
+++ rate-my-manager/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["motionarray.imgix.net"], // Add the external image domain here
+  },
   reactStrictMode: true,
 };
 
--- rate-my-manager/pages/index.js
+++ rate-my-manager/pages/index.js
@@ -13,6 +13,7 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return(
+    <div>
     <header class = "main">
       <div id= "navigation">
       <ul>
@@ -21,5 +22,16 @@ export default function Home() {
       </ul>
       </div>
     </header>
+    <div className="image-container">
+    <Image 
+          src="https://motionarray.imgix.net/preview-70417GayIL9G01E_0009.jpg?w=660&q=60&fit=max&auto=format"
+          alt="Descriptive text for the image"
+          width={800} 
+          height={400} 
+          className="rounded-lg shadow-lg"
+          />
+    </div>
+    
+    </div>
   );
 }
