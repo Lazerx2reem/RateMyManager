f2b1039b95eec9afbeeb8f464d05ba033a8655e2
Arsh Gupta <arsh121523@gmail.com>
2025-03-03 19:34:59 -0500

    Removed all components

--- rate-my-manager/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["motionarray.imgix.net"], // Add the external image domain here
-  },
-  reactStrictMode: true,
-};
-
-export default nextConfig;
